700b0b545bf79e6fa8ceb018afceebf6d8f9b1d0
DomDom <dominicfisher182001@gmail.com>
2023-06-27 18:28:39 -0400

    fixed issue where result would load immediately

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,47 @@ const rockButton = document.querySelector('.js-rock-button')
 const paperButton = document.querySelector('.js-paper-button')
 const scissorsButton = document.querySelector('.js-scissors-button')
 
-rockButton.addEventListener('click', runGame('rock'))
-paperButton.addEventListener('click', runGame('paper'))
-scissorsButton.addEventListener('click', runGame('scissors'))
+rockButton.addEventListener('click', function () {
+    runGame('rock')
+})
+paperButton.addEventListener('click', function() {
+    runGame('paper')
+})
+scissorsButton.addEventListener('click', function() {
+    runGame('scissors')
+})
+
+function runGame(playerChoice) {
+    const computerChoice = getComputerMove()
+
+    let gameResult = ''
+
+    if (playerChoice === 'rock') {
+        if (computerChoice === 'rock') {
+            console.log('Its a tie.')
+        } else if (computerChoice === 'paper') {
+            console.log('You lose :(')
+        } else if (computerChoice === 'scissors') {
+            console.log('You win!')
+        }
+    } else if (playerChoice === 'paper') {
+        if (computerChoice === 'rock') {
+            console.log('You win!')
+        } else if (computerChoice === 'paper') {
+            console.log('Its a tie.')
+        } else if (computerChoice === 'scissors') {
+            console.log('You lose :(')
+        }
+    } else if (playerChoice === 'scissors') {
+        if (computerChoice === 'rock') {
+            console.log('You lose :(')
+        } else if (computerChoice === 'paper') {
+            console.log('You win!')
+        } else if (computerChoice === 'scissors') {
+            console.log('Its a tie.')
+        }
+    }
+}
 
 function getComputerMove() {
     let randomNumber = Math.random()
@@ -20,22 +58,4 @@ function getComputerMove() {
 
     return computerChoice
 }
-
-function runGame(playerChoice) {
-    const computerChoice = getComputerMove()
-
-    let gameResult = ''
-
-    if (playerChoice === 'rock') {
-        if (computerChoice === 'rock') {
-            console.log('Its a tie.')
-        } else if (computerChoice === 'paper') {
-            console.log('You lose :(')
-        } else if (computerChoice === 'scissors') {
-            console.log('You win!')
-        }
-    }
-}
-
-
 
